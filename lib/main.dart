4275cee5412103bfadb5bb00908c490a315dc8ed
Gabriel Garcia <80141815+GbrielGarcia@users.noreply.github.com>
2021-07-15 18:39:07 -0500

    esquema

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
-import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:web_responsive_ui/screens/home/home_screens.dart';
-import 'package:web_responsive_ui/utils/style/style.dart';
 
-void main() {
-  runApp(
-      EasyDynamicThemeWidget(child: MyApp()
-      ),);
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-    debugShowCheckedModeBanner: false,
-      theme: lightThemeData,
-      darkTheme: darkThemeData,
-      themeMode: EasyDynamicTheme.of(context).themeMode,
-      home: HomeScreen(),
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
     );
   }
 }
