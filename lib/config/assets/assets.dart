9034467a5330a6d3f7deb9708cb848310ce4e61f
Gabriel Garcia <80141815+GbrielGarcia@users.noreply.github.com>
2021-07-19 23:20:06 -0500

    splash json

--- lib/config/assets/assets.dart
+++ lib/config/assets/assets.dart
@@ -1,5 +1,5 @@
 class Assets {
-  static const splasIcon = 'assets/img/welder.jpg';
   static const avatar = 'assets/img/avatar.jpg';
   static const safe = 'assets/svg/safe.svg';
+  static const splash = 'assets/json/splash.json';
 }
--- lib/pages/splash/splash.dart
+++ lib/pages/splash/splash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:lottie/lottie.dart';
+import 'package:web_responsive_ui/config/assets/assets.dart';
 // import 'package:web_responsive_ui/config/assets/assets.dart';
 import 'package:web_responsive_ui/pages/home/home.dart';
 
@@ -9,8 +10,7 @@ class SplashPages extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: AnimatedSplashScreen(
-        splash: Lottie.network(
-            'https://assets7.lottiefiles.com/datafiles/40aX5db74VvGPWw/data.json'),
+        splash: Lottie.asset(Assets.splash),
         duration: 3000,
         backgroundColor: Colors.white,
         nextScreen: HomePage(),
