34f7024040755c2464c89e269f5325d38a158893
GbrielGarcia <gabrielx2garcia@gmail.com>
2021-10-10 16:04:40 -0500

    writ l

--- lib/models/Recommendation.dart
+++ lib/models/Recommendation.dart
@@ -6,19 +6,24 @@ class Recommendation {
 
 final List<Recommendation> recommendations = [
   Recommendation(
-    name: 'Adrian Villena',
-    source: 'Cliente de Streaming Ec.',
-    text: '',
+    name: "Adrian Villena",
+    source: "Cliente de Streaming Ec.",
+    text: "",
   ),
   Recommendation(
-    name: 'Jose Huertas',
-    source: 'Cliente de Super Huertas Tecnology.',
-    text: '',
+    name: "Jose Huertas",
+    source: "Cliente de Super Huertas Tecnology.",
+    text: "",
   ),
   Recommendation(
-    name: 'Anthony Delgado',
-    source: 'Cliente de Noticas Red (Ui/Ux)',
+    name: "Anthony Delgado",
+    source: "Cliente de Noticas Red (Ui/Ux)",
     text:
-        'Me gusto el trabajo realizado ya que si cumplio con las especificaciones que le di.',
+        "Me gusto el trabajo realizado ya que si cumplio con las especificaciones que le di.",
+  ),
+  Recommendation(
+    name: "Dario Mendoza",
+    source: "Cliente de factura de consola realizado con C++.",
+    text: "La aplicacion de consola bien realizada, lo recomiendo.",
   ),
 ];
