d1365def992b0fe4adbb8fe2a808a74aae8d2297
GbrielGarcia <gabrielx2garcia@gmail.com>
2021-10-10 15:19:23 -0500

    write s

--- lib/models/Recommendation.dart
+++ lib/models/Recommendation.dart
@@ -7,12 +7,12 @@ class Recommendation {
 final List<Recommendation> recommendations = [
   Recommendation(
     name: 'Adrian Villena',
-    source: 'Cliente de Streaming Ec',
+    source: 'Cliente de Streaming Ec.',
     text: '',
   ),
   Recommendation(
     name: 'Jose Huertas',
-    source: 'Cliente de Super Huertas Tecnology',
+    source: 'Cliente de Super Huertas Tecnology.',
     text: '',
   ),
   Recommendation(
