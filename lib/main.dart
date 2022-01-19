3f0fa43a607bf7b865e968dd129b9826a70f19ed
gbrielgarcia <agguamang@itsjapon.edu.ec>
2022-01-19 12:05:08 -0500

    p

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Gabriel Garcia',
+      title: '.Gabriel Garcia.',
       theme: ThemeData.dark().copyWith(
         primaryColor: primaryColor,
         scaffoldBackgroundColor: bgColor,
