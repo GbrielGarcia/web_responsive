ba97240cfc7f15172f843f29ae35b4f29ec22dac
Gabriel Codigo <alberto-guaman@localhost.localdomain>
2022-08-29 10:36:10 -0500

    cv cv

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -48,7 +48,7 @@ class SideMenu extends StatelessWidget {
                   TextButton(
                       onPressed: () {
                         html.window.open(
-                            "https://drive.google.com/file/d/1KBv2DJwoCH_3IrSDx2MsxYdz5ZaD7PmA/view?usp=sharing",
+                            "https://drive.google.com/file/d/11Bnm5R9rE4K1_CWrKXj6DsRziGzqSmeI/view?usp=sharing",
                             "CV ");
                       },
                       child: FittedBox(
