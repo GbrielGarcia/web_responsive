53bca050225c115f35526c3980d552a927757df2
Alberto Guaman <80141815+GbrielGarcia@users.noreply.github.com>
2023-02-10 20:38:10 -0500

    ruter ruter

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:web_responsive_ui/screens/home/home_screen.dart';
 import 'package:web_responsive_ui/screens/views/privacy_policy/privacy_policy.dart';
-import 'package:web_responsive_ui/screens/views/stream/home/home_screen_stream.dart';
 
 Map<String, WidgetBuilder> getAplicationRouter() {
   return <String, WidgetBuilder>{
     HomeScreen.route: (context) => HomeScreen(),
     PrivacyPolicy.route: (context) => PrivacyPolicy(),
-    HomeScrennStream.route: (context) => HomeScrennStream()
   };
 }
